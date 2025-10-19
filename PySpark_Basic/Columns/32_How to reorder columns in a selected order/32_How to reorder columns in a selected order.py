e6abfbb917cb896627fc6a9af1b6ef38b6b11e06
enuganti <enugantisuresh12@gmail.com>
2025-10-19 13:29:39 +0530

    Add files via upload

--- PySpark_Basic/Columns/32_How to reorder columns in a selected order/32_How to reorder columns in a selected order.py
+++ PySpark_Basic/Columns/32_How to reorder columns in a selected order/32_How to reorder columns in a selected order.py
@@ -112,7 +112,25 @@ else:
 # MAGIC %md
 # MAGIC - The **strip()** method in Python is used to **remove leading (starting)** and **trailing (ending) whitespaces** characters from a given **string**.
 # MAGIC
-# MAGIC **strip():** Trim whitespace from Strings using strip(), LSTRIP, RSTRIP -> python playlist
+# MAGIC - **strip():** Trim whitespace from Strings using strip(), LSTRIP, RSTRIP -> **python playlist**
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - In Databricks, **dbutils.widgets.get("list_ordered_clms")** returns the value of the widget as a **string**.
+# MAGIC - But sometimes, you might want to pass a **Python list** (like **['id', 'name', 'age']**) into your notebook using the **widget**.
+# MAGIC - When the **widget value** is fetched, it becomes a **string**:
+# MAGIC        
+# MAGIC        widget_value = "['id', 'name', 'age']"   # notice it's a string, not a real list
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - The function **eval()** interprets the **string** as actual **Python code**.
+# MAGIC
+# MAGIC       list_ordered_clms = eval(widget_value)
+# MAGIC - it converts the **string** representation of a **list** into an **actual Python list** object.
 
 # COMMAND ----------
 
@@ -133,12 +151,28 @@ display(method_03)
 
 # COMMAND ----------
 
+default_order = df_order.columns
+print("\nInitial Order of Columns: \n", default_order)
+
 # Move "Grade" column to the front
-cols = df_order.columns
 new_order = ["Grade"] + [c for c in cols if c != "Grade"]
+print("\nNew Order of Columns: \n", new_order)
+print("\n")
 
 df_reordered_new = df_order.select(new_order)
 display(df_reordered_new)
+
+# COMMAND ----------
+
+default_order = df_order.columns
+print("\nInitial Order of Columns: \n", default_order)
+
+cols_multiReordered = ["Event_Name", "Product_ID", "Product_Group"] + [c for c in df_order.columns if c not in ["Event_Name" ,"Product_ID", "Product_Group"]]
+print("\nNew Order of Columns: \n", cols_multiReordered)
+print("\n")  # Add a blank line for spacing
+
+df_final_ord = df_order.select(cols_multiReordered)
+display(df_final_ord)
 
 # COMMAND ----------
 
@@ -149,8 +183,14 @@ display(df_reordered_new)
 # COMMAND ----------
 
 df_add_col = df_order.withColumn("country", F.lit("India"))
+display(df_add_col)
+
+default_order = df_order.columns
+print("\nInitial Order of Columns: \n", default_order)
 
 cols_reordered = ["country"] + [c for c in df_order.columns]
+print("\nNew Order of Columns: \n", cols_reordered)
+print("\n") # Add a blank line for spacing
 
 df_final_ord = df_add_col.select(cols_reordered)
 display(df_final_ord)

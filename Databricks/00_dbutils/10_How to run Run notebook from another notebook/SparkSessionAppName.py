274689d9d27e69b9d45de6acd626d34a4d3d9152
enuganti <enugantisuresh12@gmail.com>
2025-11-08 14:37:01 +0530

    Delete Databricks/00_dbutils/10_How to run Run notebook from another notebook directory

--- Databricks/00_dbutils/10_How to run Run notebook from another notebook/SparkSessionAppName.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-from pyspark.sql import SparkSession
-
-def sparkSession(app_name):
-  spark = SparkSession.builder\
-                      .appName(app_name)\
-                      .getOrCreate()
-  return spark
